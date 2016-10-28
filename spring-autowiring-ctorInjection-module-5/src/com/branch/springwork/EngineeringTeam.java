38e9bb87ee8214dd847401366666235169054183
dineshmadhup <dineshmadhup@gmail.com>
2016-10-27 20:26:46 -0700

    Spring dependecy injection with java annotation and autowiring

---
+++ spring-autowiring-ctorInjection-module-5/src/com/branch/springwork/EngineeringTeam.java
@@ -0,0 +1,32 @@
+package com.branch.springwork;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+//@Component("eng") //eng is the bean id
+@Component
+public class EngineeringTeam implements Manager {
+	
+	private EmployeeService empService;
+	
+	@Autowired
+	public EngineeringTeam(EmployeeService empService) {
+		this.empService = empService;
+	}
+	
+	//Spring will scan for a component that
+	//implements FortuneService interface
+	//our example: HappyFortuneService meets the requirements
+
+	@Override
+	public String getDailyTaskUpdate() {
+		return "Engineering Team:getDailyTaskUpdate ---All Employee is updating daily task!";
+	}
+
+	@Override
+	public String getDailySale() {
+		return empService.getMarketSaleStatus();
+	}
+
+
+}

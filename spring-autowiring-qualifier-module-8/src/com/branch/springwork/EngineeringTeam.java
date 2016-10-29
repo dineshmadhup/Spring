6685ac6bb87a3447452b6d4dd09cbc3b45a92f31
dineshmadhup <dineshmadhup@gmail.com>
2016-10-28 19:36:10 -0700

    Autowiring and Qualifiers

---
+++ spring-autowiring-qualifier-module-8/src/com/branch/springwork/EngineeringTeam.java
@@ -0,0 +1,35 @@
+package com.branch.springwork;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+//@Component("eng") //eng is the bean id
+@Component
+public class EngineeringTeam implements Manager {
+
+	// Using @Qualifier with field
+//	@Autowired
+//	@Qualifier("randomEmployeeService")
+	private EmployeeService empService;
+
+	@Autowired
+	public EngineeringTeam(@Qualifier("salesEmployeeService") EmployeeService empService) {
+		this.empService = empService;
+	}
+
+	// Spring will scan for a component that
+	// implements EmployeeService interface
+	// our example: MyEmployeeService meets the requirements
+
+	@Override
+	public String getDailyTaskUpdate() {
+		return "Engineering Team:getDailyTaskUpdate ---All Employee is updating daily task!";
+	}
+
+	@Override
+	public String getDailySale() {
+		return empService.getMarketSaleStatus();
+	}
+
+}
